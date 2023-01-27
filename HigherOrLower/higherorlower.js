ca3c95b8289fd6c52e4c2efe476769493c9a09cf
MaliErdgn <malierdgnn@gmail.com>
2023-01-27 21:26:40 +0300

    added number of guesses

--- HigherOrLower/higherorlower.js
+++ HigherOrLower/higherorlower.js
@@ -2,9 +2,11 @@ const guessInput = document.getElementById("guessInput");
 const guessSubmit = document.getElementById("guessSubmit");
 const result = document.getElementById("result");
 const lower = document.getElementById("lower");
-const higher = document.getElementById("higher");
+const higher = document.getElementById("higher")
+const amount = document.getElementById("amount");
 
 answer = Math.floor(Math.random() * 101); // random guess between 0 and 100
+let guessAmount = 0;
 function takeGuess () {
     var input = guessInput.value;
     if (input == answer) {
@@ -21,6 +23,8 @@ function takeGuess () {
         result.style.color = "red";
         higher.innerHTML +=  input + " ";
     }
+    guessAmount += 1
+    amount.innerHTML = "Number of guesses <br>" + guessAmount;   
 } 
 
 function reset() {
@@ -28,4 +32,5 @@ function reset() {
     higher.innerHTML = "Higher <br>"
     lower.innerHTML = "Lower <br>"
     result.innerHTML = ""
+    amount.innerHTML = "Number of guesses <br>"
 }
