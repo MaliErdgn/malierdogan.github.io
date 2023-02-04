153e63f7d1f82d91d8349356864dff5ae888c443
MaliErdgn <105178494+MaliErdgn@users.noreply.github.com>
2023-02-04 16:23:58 +0300

    making mainpage divs clickable and adding a general css file

---
+++ script.js
@@ -0,0 +1,32 @@
+const temperatureConverter = document.getElementById("TemperatureConverter");
+const rps = document.getElementById("rps") // rockpaperscissors
+const hlgame = document.getElementById("hlgame") //higher or lower game
+const ttt = document.getElementById("ttt") // tictactoe
+const bros = document.getElementById("bros") //bros
+
+
+
+temperatureConverter.addEventListener("click", temperatureConverterRedirect)
+function temperatureConverterRedirect() {
+    window.location.replace("./TempatureConverter/TempatureConverter.html")
+}
+
+rps.addEventListener("click", rpsRedirect)
+function rpsRedirect() { 
+    window.location.replace("./RockPaperScissors/rockpaperscissors.html")
+}
+
+hlgame.addEventListener("click", hlgameRedirect)
+function hlgameRedirect() {
+    window.location.replace("./HigherOrLower/higherorlower.html")
+}
+
+ttt.addEventListener("click", tttRedirect)
+function tttRedirect() {
+    window.location.replace("./tictactoe/tictactoe.html")
+}
+
+bros.addEventListener("click", brosRedirect)
+function brosRedirect(){
+    window.location.replace("./bros/broslogin.html")
+}
