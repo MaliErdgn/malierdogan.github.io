e4c6db2022d1589c796c60dd445b5fe77adb5402
MaliErdgn <105178494+MaliErdgn@users.noreply.github.com>
2023-02-14 03:50:27 +0300

    UI uptade on rock paper scissors

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -1,3 +1,13 @@
+const mainMenu = document.getElementById("main-menu");
+const _rock = document.getElementById("rock");
+const _paper = document.getElementById("paper");
+const _scissors = document.getElementById("scissors");
+const _reset = document.getElementById("reset");
+
+mainMenu.addEventListener("click",goToMainMenu)
+function goToMainMenu() {
+  window.location.replace("/index.html")
+}
 let playerScore = 0;
 let cpuScore = 0;
 let choices = ["rock","paper","scissors"]
@@ -6,21 +16,28 @@ function scoreboard(cpu) {
     document.getElementById("result").innerHTML = "Computer Choose: " + cpu; 
 }
 
+_rock.addEventListener("click", rock)
 function rock(){
+    console.log("rock");
     let cpuChoice = choices[Math.floor(Math.random() * choices.length)]
-     if (cpuChoice == "rock") {}
-     if (cpuChoice == "paper") {cpuScore += 1}
-     if (cpuChoice == "scissors") {playerScore += 1}
+    if (cpuChoice == "rock") {}
+    if (cpuChoice == "paper") {cpuScore += 1}
+    if (cpuChoice == "scissors") {playerScore += 1}
     scoreboard(cpuChoice);
-    }
+}
+
+
+_paper.addEventListener("click", paper)
 function paper(){
     let cpuChoice = choices[Math.floor(Math.random() * choices.length)]
     if (cpuChoice == "rock") {playerScore += 1}
     if (cpuChoice == "scissors") {cpuScore += 1}
     if (cpuChoice == "paper") {}    
     scoreboard(cpuChoice);
-    }
+}
 
+
+_scissors.addEventListener("click",scissors)
 function scissors(){
     let cpuChoice = choices[Math.floor(Math.random() * choices.length)]
     if (cpuChoice == "rock") {cpuScore += 1}
@@ -28,8 +45,9 @@ function scissors(){
     if (cpuChoice == "scissors") {}
     scoreboard(cpuChoice);
 
-    }
+}
 
+_reset.addEventListener("click",reset)
 function reset () {
     playerScore = 0;
     cpuScore = 0;
