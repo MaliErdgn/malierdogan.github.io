1bf7a7dfdabeb28d77c5ec8dfa487a2f560a61c6
MaliErdgn <malierdgnn@gmail.com>
2023-02-22 00:39:41 +0300

    trying to make the slide effect

--- ProjectEuler/projecteuler.js
+++ ProjectEuler/projecteuler.js
@@ -1,18 +1,25 @@
 const mainMenu = document.getElementById("main-menu");
 const answer1 = document.getElementById("answer1")
 const one = document.getElementById("one");
-const explanation = document.getElementById("explanation");
+const explanation = document.querySelector(".explanation")
 
 mainMenu.addEventListener("click",goToMainMenu)
 function goToMainMenu() {
   window.location.replace("/index.html")
 }
 
+var isExplanationOn = false;
 one.addEventListener("click", function(){
-  if (explanation.style.display == "none"){
-  explanation.style.display = "block"
+  if (isExplanationOn == false){
+    explanation.style.height = "auto"
+    explanation.style.width = "inherit"
+    explanation.classList.add("fadeIn")
+    isExplanationOn = true;
   } else {
-    explanation.style.display = "none"
+    explanation.style.width = "0"
+    explanation.style.height = "0"
+    explanation.classList.remove("fadeIn")
+    isExplanationOn = false;
   }
 })
 
